163938c3d3b68a9b9698d97813936d763eb9bdf8
Izaias <Izaias@BATTOUSAI>
2017-10-16 18:47:10 -0200

    Tratamento string

--- Videos/Models/Repository/MusicaRepository.cs
+++ Videos/Models/Repository/MusicaRepository.cs
@@ -5,6 +5,7 @@ using Videos.Models.Entity;
 namespace Videos.Models.Repository {
     public class MusicaRepository : BaseRepository {
         public musica GetMusicaByTitulo(string titulo) {
+            titulo = System.Web.HttpUtility.HtmlDecode(titulo);
             musica musica = db.musica.Where(m => m.titulo == titulo).FirstOrDefault();
             if (musica == null) {
                 musica = AdicionarMusica(titulo);
